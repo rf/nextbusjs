f342b14cb8538fda2b91a10810c25247320941c2
Russell Frank <russ.frank@rutgers.edu>
2012-01-31 22:33:46 -0500

    adding sorted route & stops lists, and accessors

--- test/agencycache.js
+++ test/agencycache.js
@@ -10,33 +10,11 @@ var vows       = require('vows'),
 
 var suite = vows.describe('just agency cache');
 
-var isValidPredictions = function (err, data) {
-   var item;
-//   console.log(data);
-   if (err) {
-      throw err;
-   }
-   if (data !== null) {
-      assert.isObject(data);
-      for (item in data) {
-         if (data.hasOwnProperty(item)) {
-            if (data[item] !== null) {
-               assert.isArray(data[item]);
-               data[item].forEach(function (val) {
-                  assert.isNumber(Number(val));
-               });
-            }
-         }
-      }
-   } else {
-      assert.isNull(data);
-   }
-};
-
 suite.addBatch({
    'rutgers' : {
       topic    : function () { rutgers.cacheAgency('rutgers', this.callback); },
       'doesnt break' : function (topic) {
+         //console.dir(topic);
          assert.isObject(topic);
       }
    }
